0ef0223b1e0ec13b6806b08509c4ea50a48c24fc
Wai Phyo <waiphyo@globalsignin.com>
2014-02-23 09:04:15 +0630

    1st commit

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+</article>
+</section>
+</div>
+<div class="body2">
+    <div class="main">
+        <footer>
+            <div class="footerlink">
+                <p class="lf">Copyright &copy; 2010 <a href="#">Domain Name</a> - All Rights Reserved</p>
+                <p class="rf"><a href="http://www.free-css.com/">Free CSS Templates</a> by <a href="http://www.templatemonster.com/">TemplateMonster</a></p>
+                <div style="clear:both;"></div>
+            </div>
+        </footer>
+    </div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+<!-- END PAGE SOURCE -->
+</body>
+</html>
