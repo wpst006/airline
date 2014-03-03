fe42b833ec9ee17b8557fc79f2682e4bd5c341e7
Wai Phyo <waiphyo@globalsignin.com>
2014-03-03 17:23:52 +0630

    schedule edit

---
+++ includes/dateTimeHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+class dateTimeHelper {
+
+    public static function getDateTimeForUI($stringValue = null) {
+        if ($stringValue == null) {
+            return date('Y-m-d h:i A');
+        } else {
+            return date('Y-m-d h:i A', strtotime($stringValue));
+        }
+    }
+
+}
+
+?>
--- schedule.php
+++ schedule.php
@@ -1,14 +1,19 @@
 <?php include('includes/includefiles.php'); ?>
 <?php require_once("includes/routeHelper.php"); ?>
 <?php require_once("includes/flightHelper.php"); ?>
+<?php require_once("includes/scheduleHelper.php"); ?>
 
 <?php
 if (isset($_POST['submitted'])) {
     //var_dump($_POST);exit();
-    
     //*********************************************************************
     //Filling Data
-    $seat_id = autoID::getAutoID('schedules', 'schedule_id', 'SCH_', 6);
+    if (isset($_POST['schedule_id'])) {
+        $schedule_id = $_POST['schedule_id'];
+    } else {
+        $schedule_id = autoID::getAutoID('schedules', 'schedule_id', 'SCH_', 6);
+    }
+
     $flight_id = $_POST['flight_id'];
     $route_id = $_POST['route_id'];
     $departure_datetime = $_POST['departure_datetime'];
@@ -17,18 +22,56 @@ if (isset($_POST['submitted'])) {
     $arrival_airport = $_POST['arrival_airport'];
     $remark = $_POST['remark'];
     //*********************************************************************
-    $departure_datetime_string=date('Y-m-d H:i:00',strtotime($departure_datetime));
-    $arrival_datetime_string=date('Y-m-d H:i:00',strtotime($arrival_datetime));
-    //"schedules" Table Insert
-    $scheduleInsert_sql = "INSERT INTO " .
-            "schedules(schedule_id,flight_id,route_id,departure_datetime,arrival_datetime,departure_airport,arrival_airport,active,remark) " .
-            "VALUES('$seat_id','$flight_id','$route_id','$departure_datetime_string','$arrival_datetime_string','$departure_airport','$arrival_airport',1,'$remark')";
+    $departure_datetime_string = date('Y-m-d H:i:00', strtotime($departure_datetime));
+    $arrival_datetime_string = date('Y-m-d H:i:00', strtotime($arrival_datetime));
+    //*********************************************************************
+    if (isset($_POST['schedule_id'])) {
+        //"schedules" Table Update
+        $schedule_sql = "Update " .
+                "schedules " . 
+                "SET departure_datetime='" . $departure_datetime_string . "', " .
+                "arrival_datetime='" . $arrival_datetime_string . "', " .
+                "departure_airport='" . $departure_airport . "', " .
+                "arrival_airport='" . $arrival_airport . "', " .
+                "remark='" . $remark . "' " .
+                "WHERE schedule_id='" . $schedule_id . "'";
+    } else {
+        //"schedules" Table Insert
+        $schedule_sql = "INSERT INTO " .
+                "schedules(schedule_id,flight_id,route_id,departure_datetime,arrival_datetime,departure_airport,arrival_airport,active,remark) " .
+                "VALUES('$schedule_id','$flight_id','$route_id','$departure_datetime_string','$arrival_datetime_string','$departure_airport','$arrival_airport',1,'$remark')";
+    }
 
-    mysql_query($scheduleInsert_sql) or die(mysql_error());
+    mysql_query($schedule_sql) or die(mysql_error());
     //*********************************************************************   
     messageHelper::setMessage("You have successfully add schedule.", MESSAGE_TYPE_SUCCESS);
     header("Location:route-detail-display.php?route_id=" . $_POST['route_id']);
     exit();
+} else {
+    $departure_datetime = date('Y-m-d h:i A', time());
+    $arrival_datetime = date('Y-m-d h:i A', time());
+    $departure_airport = "";
+    $arrival_airport = "";
+    $remark = "";
+    $schedule_id = null;
+
+    if (isset($_GET['schedule_id'])) {
+        $schedule_id = $_GET['schedule_id'];
+
+        $scheduleData = scheduleHelper::getScheduleByScheduleID($schedule_id);
+
+        //var_dump($scheduleData);exit();
+        $flight_id = $scheduleData[0]['flight_id'];
+        $route_id = $scheduleData[0]['route_id'];
+        $departure_datetime = $scheduleData[0]['departure_datetime'];
+        $arrival_datetime = $scheduleData[0]['arrival_datetime'];
+        $departure_airport = $scheduleData[0]['departure_airport'];
+        $arrival_airport = $scheduleData[0]['arrival_airport'];
+        $remark = $scheduleData[0]['remark'];
+    } else {
+        $flight_id = $_GET['flight_id'];
+        $route_id = $_GET['route_id'];
+    }
 }
 ?>
 
@@ -38,27 +81,31 @@ if (isset($_POST['submitted'])) {
     <div class="col-md-12">
         <form role="form" id="schedule" name="schedule" action="schedule.php" method="post" class="form-horizontal">         
 
+            <?php if (isset($schedule_id)) { ?>
+                <input type="hidden" id="schedule_id" name="schedule_id" value="<?php echo $schedule_id; ?>" />
+            <?php } ?>
+
             <div class="form-group">
                 <label class="col-sm-3 control-label">Flight :</label>
                 <div class="col-sm-9">
-                    <p class="form-control-static"><?php echo flightHelper::getFlightNameByFligthID($_GET['flight_id']); ?></p>
-                    <input type="hidden" id="flight_id" name="flight_id" value="<?php echo $_GET['flight_id']; ?>" />
+                    <p class="form-control-static"><?php echo flightHelper::getFlightNameByFligthID($flight_id); ?></p>
+                    <input type="hidden" id="flight_id" name="flight_id" value="<?php echo $flight_id; ?>" />
                 </div>                            
             </div>
-            
+
             <div class="form-group">
                 <label class="col-sm-3 control-label">Route :</label>
                 <div class="col-sm-9">
-                    <p class="form-control-static"><?php echo routeHelper::getRouteTitleByRouteID($_GET['route_id']); ?></p>
-                    <input type="hidden" id="route_id" name="route_id" value="<?php echo $_GET['route_id']; ?>" />
+                    <p class="form-control-static"><?php echo routeHelper::getRouteTitleByRouteID($route_id); ?></p>
+                    <input type="hidden" id="route_id" name="route_id" value="<?php echo $route_id; ?>" />
                 </div>                            
             </div>
-            
+
             <div class="form-group">
                 <div class="col-sm-3 control-label">Departure Date :</div>
                 <div class="col-sm-9">
                     <div class='input-group date' id='datetimepicker1'>
-                        <input type='text' id="departure_datetime" name="departure_datetime" class="form-control" data-format="YYYY-MM-DD hh:mm A" value="<?php echo date('Y-m-d h:i A', time()); ?>"/>
+                        <input type='text' id="departure_datetime" name="departure_datetime" class="form-control" data-format="YYYY-MM-DD hh:mm A" value="<?php echo $departure_datetime; ?>"/>
                         <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
                         </span>
                     </div>
@@ -69,7 +116,7 @@ if (isset($_POST['submitted'])) {
                 <div class="col-sm-3 control-label">Arrival Date :</div>
                 <div class="col-sm-9">
                     <div class='input-group date' id='datetimepicker1'>
-                        <input type='text' id="arrival_datetime" name="arrival_datetime" class="form-control" data-format="YYYY-MM-DD hh:mm A" value="<?php echo date('Y-m-d h:i A', time()); ?>"/>
+                        <input type='text' id="arrival_datetime" name="arrival_datetime" class="form-control" data-format="YYYY-MM-DD hh:mm A" value="<?php echo $arrival_datetime; ?>"/>
                         <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span>
                         </span>
                     </div>
@@ -87,21 +134,21 @@ if (isset($_POST['submitted'])) {
             <div class="form-group">
                 <div class="col-sm-3 control-label">Departure Airport :</div>
                 <div class="col-sm-9">
-                    <input type="text" id="departure_airport" name="departure_airport" class="form-control" value="" maxlength="30"/>
+                    <input type="text" id="departure_airport" name="departure_airport" class="form-control" value="<?php echo $departure_airport; ?>" maxlength="30"/>
                 </div>
             </div>
 
             <div class="form-group">
                 <div class="col-sm-3 control-label">Arrival Airport :</div>
                 <div class="col-sm-9">
-                    <input type="text" id="arrival_airport" name="arrival_airport" class="form-control" value="" maxlength="30"/>
+                    <input type="text" id="arrival_airport" name="arrival_airport" class="form-control" value="<?php echo $arrival_airport; ?>" maxlength="30"/>
                 </div>
             </div>
 
             <div class="form-group">
                 <div class="col-sm-3 control-label">Remark :</div>
                 <div class="col-sm-9">
-                    <textarea id="remark" name="remark" class="form-control" rows="3"></textarea>                    
+                    <textarea id="remark" name="remark" class="form-control" value="<?php echo $remark; ?>" rows="3"></textarea>                    
                 </div>
             </div>            
 
